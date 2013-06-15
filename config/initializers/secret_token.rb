6d4b865f115163b1712bc8a9ea1e12a1589e5220
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-06-15 12:46:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Terribletracker::Application.config.secret_token = 'b955e7909bf9f2f89ad987cd40057ebca44ec96ffeb605cb7c63e1eda747db8f3e06e6c4bfcc015189b3fde32b30772aef0e2f549c6253b5a8581b0c4d709422'
