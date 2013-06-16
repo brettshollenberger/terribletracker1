efe0dccea0b44c8668cb1ae520177418623a571e
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-06-16 15:46:26 -0400

    Add nav

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Terribletracker::Application.routes.draw do
 
   root to: "projects#index"
 
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+    get "/logout" => "devise/sessions#destroy"
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
